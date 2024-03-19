b9ea9363b0ca70fb8d6e9c5aded2c6946fe57ac4
SpringsEternal <zenomorandi@gmail.com>
2024-03-19 13:03:15 +0100

    Propping work in progress
    così siamo tutti on the piece

--- Assets/scripts/mouse.cs
+++ Assets/scripts/mouse.cs
@@ -15,19 +15,20 @@ public class mouse : MonoBehaviour
     public float interactionRange = 50f; // Distanza per interagire con gli oggetti
     public Transform playerMesh;
     public Transform camera;
+    int layerMask=1<<6;
     
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        layerMask = ~layerMask;
         //Cursor.lockState = CursorLockMode.Confined;
     }
 
     // Update is called once per frame
     void Update()
     {
-        int layerMask=1<<6;
-        layerMask = ~layerMask;
+        
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
  
@@ -37,28 +38,7 @@ public class mouse : MonoBehaviour
         transform.transform.eulerAngles = new Vector3(xRotation, transform.rotation.eulerAngles.y, 0f);
         transform.Rotate(Vector3.up * mouseX);
 
-        Ray ray = new Ray(camera.position, camera.forward);
-        RaycastHit hit;
-        
-        Debug.DrawRay(camera.position, camera.forward*interactionRange,Color.green);
-        // Se il raycast colpisce un oggetto entro tot range
-        if (Physics.Raycast(ray, out hit, interactionRange,layerMask))
-        {
-            // Controllo se l'oggetto colpito è interagibile e se il tasto viene premuto
-            if (hit.collider.CompareTag("Trasformabile") && Input.GetKeyDown(trasformationKey))
-            {
-                // Cambia la mesh dell'oggetto prop1test
-                MeshFilter objectMesh = prop1test.GetComponent<MeshFilter>();
-                MeshFilter playerMeshFilter = playerMesh.GetComponent<MeshFilter>();
-
-                // Se entrambi gli oggetti hanno una mesh
-                if (objectMesh != null && playerMeshFilter != null)
-                {
-                    //Viene applicata la mesh di prop1test al player
-                    playerMeshFilter.mesh = objectMesh.sharedMesh;
-                }
-            }
-        }
+        Trasformazione();
 /*
         // vettori per la camera e per puntare gli oggetti non ci ho capito una secchia quello che ho scritto funziona basta dilli che quando vede la sfera si trasforma
         Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -72,5 +52,37 @@ public class mouse : MonoBehaviour
             }
         }
 */
+    }
+    void Trasformazione()
+    {
+        Ray ray = new Ray(camera.position, camera.forward);
+        RaycastHit hit;
+        
+        Debug.DrawRay(camera.position, camera.forward*interactionRange,Color.green);
+        // Se il raycast colpisce un oggetto entro tot range
+        if (Physics.Raycast(ray, out hit, interactionRange,layerMask))
+        {
+            // Controllo se l'oggetto colpito è interagibile e se il tasto viene premuto
+            if (hit.collider.CompareTag("Trasformabile") && Input.GetKeyDown(trasformationKey))
+            {
+                // Cambia la mesh dell'oggetto prop1test
+                MeshFilter objectMesh = hit.collider.gameObject.GetComponent<MeshFilter>();
+                MeshFilter playerMeshFilter = playerMesh.GetComponent<MeshFilter>();
+                Renderer render=hit.collider.GetComponent<Renderer>();
+                // Se entrambi gli oggetti hanno una mesh
+                
+
+                if (objectMesh != null && playerMeshFilter != null)
+                {
+                    //Viene applicata la mesh di prop1test al player
+                    playerMeshFilter.mesh = objectMesh.sharedMesh;
+                    List<Material> m=new List<Material>();
+                    render.GetMaterials(m);
+                    Renderer pRender=playerMesh.GetComponent<Renderer>();
+                    pRender.SetMaterials(m);
+                }
+            }
+        }
+
     }
 }
